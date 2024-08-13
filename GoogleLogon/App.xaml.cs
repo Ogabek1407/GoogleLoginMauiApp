9183516309e7fbd13be534976f7777fa456817e3
Ogabek1407 <oagbeksaparov1407@gmail.com>
2024-08-13 16:19:01 +0500

    first comment

---
+++ GoogleLogon/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GoogleLogon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
